e248900eb9975be1bb39c7129dc636b2ae7b1da1
Shivam Kumar Bari <68389152+ShivamBari2728@users.noreply.github.com>
2026-03-16 10:47:11 +0000

    routing in node

---
+++ Node js/node server/routing/coreModule.js
@@ -0,0 +1,37 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        fs.readFile('index.html', (err, data) => {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(data);
+            res.end();
+        });
+    }
+    else if (req.url === '/about') {
+        fs.readFile('about.html', (err, data) => {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(data);
+            res.end();
+        });
+    }
+   else if (req.url === '/contact') {
+        fs.readFile('contact.html', (err, data) => {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(data);
+            res.end();
+        });
+    }
+    else if (req.url === '/home'){
+        fs.readFile('index.html', (err, data) => {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(data);
+            res.end();
+        });
+    }
+
+});
+
+server.listen(3000);
+console.log('Server is running on http://localhost:3000');
