5297ae594cde06487098f4f1f660ca0d2d7b6d6c
Shivam Kumar Bari <68389152+ShivamBari2728@users.noreply.github.com>
2026-03-18 08:26:27 +0000

    routing usinig express

---
+++ Express_Ex/routing_/expressModule.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'Home.html'))
+})
+app.get('/contact',(req,res)=>{
+    res.sendFile(path.join(__dirname,'Contact.html'))
+})
+app.get('/about',(req,res)=>{
+    res.sendFile(path.join(__dirname,'About.html'))
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on http://localhost:${PORT}`);
+})
